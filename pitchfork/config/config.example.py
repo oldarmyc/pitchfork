3c393273cb9f36acbb5a7c7ccf6bfc4c0c1dee3a
davi4261 <david.kludt@rackspace.com>
2014-03-25 15:19:44 -0500

    Update example config

--- pitchfork/config/config.example.py
+++ pitchfork/config/config.example.py
@@ -4,12 +4,11 @@ import os
 
 MONGO_HOST = 'localhost'
 MONGO_PORT = 27017
-MONGO_USER = None
-MONGO_PASS = None
-MONGO_KWARGS = None
+MONGO_KWARGS = {'tz_aware': True}
+MONGO_DATABASE = 'pitchfork'
 
 ADMIN = 'cloud_username'
-ADMIN_NAME = 'Full Name'
+ADMIN_NAME = 'Admin Full Name'
 
-SECRET_KEY = 'secret_key'
+SECRET_KEY = 'secret_key_for_cookie'
 LOG_PATH = os.path.join(os.path.dirname(__file__), 'logs/devel.log')
