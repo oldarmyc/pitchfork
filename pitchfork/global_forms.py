81f1c09c37fa1de080e6f5bf56e28ca26381c943
davi4261 <david.kludt@rackspace.com>
2014-09-12 07:41:13 -0500

    Add import

--- pitchfork/global_forms.py
+++ pitchfork/global_forms.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from flask import g
 from flask.ext.wtf import Form
 from wtforms import fields, validators
 
