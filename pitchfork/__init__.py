eb906c05525b1cb2d28cfd25784223c710593f47
davi4261 <david.kludt@rackspace.com>
2014-09-15 10:42:38 -0500

    Cleanup and add template functions

--- pitchfork/__init__.py
+++ pitchfork/__init__.py
@@ -81,22 +81,21 @@ def check_regex(value):
 
 @app.context_processor
 def utility_processor():
+    def unslug(string):
+        return re.sub('_', ' ', string)
+
     def parse_field_data(value):
         choices = re.sub('\r\n', ',', value)
         return choices.split(',')
 
-    def to_json(value):
-        temp = json.dumps(
-            value,
-            sort_keys=False,
-            indent=4,
-            separators=(',', ':')
-        )
-        return temp
+    def slugify(data):
+        temp_string = re.sub(' +', ' ', str(data.strip()))
+        return re.sub(' ', '_', temp_string)
 
     return dict(
         parse_field_data=parse_field_data,
-        to_json=to_json
+        unslug=unslug,
+        slugify=slugify
     )
 
 
