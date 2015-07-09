40e1fac2ce217733d9d99911a11f03f1ee8ae172
davi4261 <david.kludt@rackspace.com>
2015-07-09 13:36:24 -0500

    Add in copyright to test files

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,3 +1,17 @@
+# Copyright 2014 Dave Kludt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from fixtures.authenticate import auth_return
@@ -22,8 +36,8 @@ class SeleniumTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         try:
-            # cls.client = webdriver.PhantomJS(service_log_path=os.path.devnull)
-            cls.client = webdriver.Firefox()
+            cls.client = webdriver.PhantomJS(service_log_path=os.path.devnull)
+            # cls.client = webdriver.Firefox()
         except:
             pass
 
